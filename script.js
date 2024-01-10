9dff2a31ddc306402792f8c7bae509f6bda27faa
tarasiuk2809 <tarasiuk.katsiaryna@gmail.com>
2024-01-10 15:07:20 +0300

    Домашнее задание 4
    Удаление неиспользуемых переменных

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const sendInput = document.querySelector('#input');
 const items = document.querySelector('.items');
-let itemsArray = Array.from(items);
-const allItems = document.getElementsByClassName('item');
-let itemText = null;
 
 sendInput.addEventListener('keydown', function (event) {
   if (event.key == 'Enter') {
-    itemText = sendInput.value;
+    let itemText = sendInput.value;
     let newItem = document.createElement('div');
     newItem.classList.add('item');
     newItem.textContent = itemText;
